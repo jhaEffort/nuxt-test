b5b113e7999731b4c68ef87b22535ce38bd8bb74
jhahahaha <46337068+jhahahaha@users.noreply.github.com>
2021-02-04 20:32:14 +0800

    Update push.js

--- push.js
+++ push.js
@@ -1,11 +1,6 @@
 
 /*
-*argv: ['node地址', '执行文件', '命令行参数']
-<<<<<<< HEAD
-*222222
-=======
-*999999999999
->>>>>>> 84811b523b0fc7e84389a012fc81dfdcdd549891
+*argv: ['node地址', '执行文件', '命令行参数']222
 */
 // console.log(process.argv.splice(2))
 const childProcess = require('child_process')
