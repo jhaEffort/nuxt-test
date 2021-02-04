a585e1add2474e200958f568a68dd033556746ad
jha <915885979@qq.com>
2021-02-04 19:44:32 +0800

    alter

--- test.sh
+++ test.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
-# msg=$1
-# branch=$2
-error=$3
+msg=$1
+branch=$2
+# error=$3
 # echo ${msg} ${branch}
-# if [ -n "$msg" ]; then
-#    git add -A
-#    git commit -m"${msg}"
-#    git fetch origin "${branch}"
-#    git pull origin "${branch}"
-#    git push origin "${branch}"
-#    npm run dev
-#     echo "项目已启动"
-# else
-#     echo "请添加注释再来一遍"
-# fi
-
-git pull origin master
-
-if [ -n "$error" ]; then
-   echo $error
- else
-   echo "成功了"
+if [ -n "$msg" ]; then
+   git add -A
+   git commit -m"${msg}"
+   git fetch origin "${branch}"
+   git pull origin "${branch}"
+    if [ $? == 0]; then
+      git push origin "${branch}"
+      npm run dev
+      echo "项目已启动"
+    else
+      echo "请解决冲突"
+    fi
+else
+    echo "请添加注释再来一遍"
 fi
+
+# git pull origin master
+
+# if [ -n "$error" ]; then
+#    echo $error
+#  else
+#    echo "成功了"
+# fi
 # aa=$(git add -A 2>&1)
 # echo $aa
 # cat: /tmp/aa: No such file or directory
