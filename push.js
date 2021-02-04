6ed45bc89eff45c8daa6e1d96f00925720c679f0
jha <915885979@qq.com>
2021-02-04 11:48:04 +0800

    alter

--- push.js
+++ push.js
@@ -5,7 +5,7 @@
 // console.log(process.argv.splice(2))
 const childProcess = require('child_process')
 const branch = childProcess.execSync('git rev-parse --abbrev-ref HEAD').toString().replace(/\s+/, '')
-console.log('去111111', branch)
+console.log('当前分支名', branch)
 const { spawn, exec, execFile } = require('child_process');
 
 let syncFile = spawn('sh', ['./test.sh']);
