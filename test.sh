4acc097129b94129112cb4d74ee673ca5630d637
jha <915885979@qq.com>
2021-02-04 18:24:39 +0800

    alter

--- test.sh
+++ test.sh
@@ -15,14 +15,13 @@ error=$3
 #     echo "请添加注释再来一遍"
 # fi
 
-
+aa = git pull origin master
+echo {$aa}
 if [-n "$error"]; then
   echo "错误了"
 else
   echo "成功了"
 fi
-aa = git pull origin master
-echo {$aa}
 # aa=$(git add -A 2>&1)
 # echo $aa
 # cat: /tmp/aa: No such file or directory
