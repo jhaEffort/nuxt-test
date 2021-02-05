5357c79d31ba681c41e7bdd871bfb07be5b7e0b8
jha <915885979@qq.com>
2021-02-05 11:26:09 +0800

    fix

--- test.sh
+++ test.sh
@@ -2,29 +2,27 @@
 msg=$1
 branch=$2
 num=0
-arg= $(git ls-files -u | cut -f 2 | sort -u)
-echo '000000000000000000000000000000'
+arg= `git ls-files -u | cut -f 2 | sort -u`
 echo ${arg}
-echo '000000000000000000000000000000'
-if [ -n "$msg" ]; then
+# if [ -n "$msg" ]; then
 
-   git add -A
-   git commit -m"${msg}"
-   git fetch origin "${branch}"
-   git pull origin "${branch}"
-   git push origin "${branch}"
-   flag=  $?
-    if [ $flag -eq "$num" ]; then
-      echo "输出的$flag"
-      npm run dev
-      echo "项目已启动"
-    else
-      echo "else中$flag"
-      echo "请先解决冲突"
-    fi
-else
-    echo "请添加注释再来一遍"
-fi
+#    git add -A
+#    git commit -m"${msg}"
+#    git fetch origin "${branch}"
+#    git pull origin "${branch}"
+#    git push origin "${branch}"
+#    flag=  $?
+#     if [ $flag -eq "$num" ]; then
+#       echo "输出的$flag"
+#       npm run dev
+#       echo "项目已启动"
+#     else
+#       echo "else中$flag"
+#       echo "请先解决冲突"
+#     fi
+# else
+#     echo "请添加注释再来一遍"
+# fi
 
 # $? 上次代码执行结果 0 是成功 1 是失败
 
